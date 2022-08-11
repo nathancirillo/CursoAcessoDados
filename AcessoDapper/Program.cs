b33e71cce1a30502661ad5fc766a33388ee7d770
Nathan Cirillo <cirillo.nathan@gmail.com>
2022-08-11 17:21:50 -0300

    novas implementações

--- AcessoDapper/Program.cs
+++ AcessoDapper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using Dapper;
 using CursoAcessoDados.Models;
+using System.Data;
 
 namespace AcessoDapper
 {
@@ -9,13 +10,25 @@ namespace AcessoDapper
         public static void Main(string[]args)
         {
             const string strConnection = "Server=TARGETN107\\SQLEXPRESS; Database=balta; User ID=sa; Password=123456; Encrypt=False;";
-                        
+                       
+            using(var connection = new SqlConnection(strConnection))
+            {
+                // CreateCategory(connection);
+                // CreateManyCategories(connection);
+                // UpdateCategory(connection);
+                // ListCategories(connection);  
+                ExecuteProcedure(connection);              
+            }
+        }
+
+        private static void CreateCategory(SqlConnection connection)
+        {
             var newCategory = new Category()
             {
                 Id = Guid.NewGuid(),
-                Title = "Amazon AWS",
-                Url = "amazon",
-                Description = "Categoria destinada a serviços do AWS", 
+                Title = $"Curso Aleatório {Guid.NewGuid().ToString().Substring(0,5)}",
+                Url = "aleatorio",
+                Description = "Curso aleatorio destinado a programação", 
                 Order = 8,
                 Summary = "AWS Cloud",
                 Featured = false
@@ -34,11 +47,8 @@ namespace AcessoDapper
                                    @Featured     
                                 )
                             ";
-            
-            
-            using(var connection = new SqlConnection(strConnection))
-            {
-                var rowsAffected = connection.Execute(insertSQL, new 
+                            
+             var rowsAffected = connection.Execute(insertSQL, new 
                 {
                     newCategory.Id,
                     newCategory.Title,
@@ -48,14 +58,100 @@ namespace AcessoDapper
                     newCategory.Summary,
                     newCategory.Featured
                 });
-                System.Console.WriteLine($"{rowsAffected} linhas inseridas.");
+                
+            System.Console.WriteLine($"{rowsAffected} linhas inseridas.");
 
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");     
-                foreach(var category in categories)
-                {
-                    System.Console.WriteLine($"{category.Id} - {category.Title}." );
-                }  
-            }
         }
+    
+        private static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");     
+            foreach(var category in categories)
+            {
+                System.Console.WriteLine($"{category.Id} - {category.Title}." );
+            }  
+        }
+
+        private static void UpdateCategory(SqlConnection connection)
+        {
+            var strUpdate = @"UPDATE [Category] SET [Title] = @newTitle WHERE [Id] = @id";
+            var updatedRows = connection.Execute(strUpdate, new 
+            {
+                id = "03d21ec7-cfa6-4fbb-b560-368a473c3de8",
+                newTitle = "Título da categoria alterado"        
+            });
+            System.Console.WriteLine($"{updatedRows} linhas alteradas. ");
+        }
+        
+        private static void CreateManyCategories(SqlConnection connection)
+        {
+            var strInsert = @" INSERT INTO [CATEGORY] 
+                               VALUES 
+                               ( 
+                                  @Id, 
+                                  @Title, 
+                                  @Url, 
+                                  @Summary, 
+                                  @Order, 
+                                  @Description, 
+                                  @Featured
+                               )";
+
+             var categoriaUm = new Category()
+             {
+                Id = Guid.NewGuid(),
+                Title = "Many Category One",
+                Description = "Example of create many categories",
+                Url = "manycategories",
+                Featured = false,
+                Order = 1,
+                Summary = "Many Categories"
+             };
+
+             var categoriaDois= new Category()
+             {
+                Id = Guid.NewGuid(),
+                Title = "Many Category Two",
+                Description = "Example of create many categories",
+                Url = "manycategories",
+                Featured = false,
+                Order = 1,
+                Summary = "Many Categories"
+             };
+
+             var rowsInserted = connection.Execute(strInsert, new[]
+             {
+                new 
+                {
+                  categoriaUm.Id,
+                  categoriaUm.Title,
+                  categoriaUm.Url,
+                  categoriaUm.Summary,
+                  categoriaUm.Order,
+                  categoriaUm.Description,
+                  categoriaUm.Featured
+                },
+                new 
+                {
+                  categoriaDois.Id,
+                  categoriaDois.Title,
+                  categoriaDois.Url,
+                  categoriaDois.Summary,
+                  categoriaDois.Order,
+                  categoriaDois.Description,
+                  categoriaDois.Featured
+                }
+             });
+             System.Console.WriteLine($"{rowsInserted} linhas inseridas.");
+        }
+
+        private static void ExecuteProcedure(SqlConnection connection)
+        {
+            var strProcedure = "[spExcluiContaAluno]";
+            var pars = new { StudentId = "7232054c-0fdd-4969-9524-b56e609e8e57"};  
+            var rowsAffected = connection.Execute(strProcedure, pars, commandType: CommandType.StoredProcedure);
+            System.Console.WriteLine($"{rowsAffected} registros excluídos.");
+        }
+
     }
 }
